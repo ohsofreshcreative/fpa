5f612969c312a36a6c33a8b3176763e54af672e3
Arek <kontakt@ohsofresh.pl>
2026-04-20 13:30:13 +0200

    Automatyczny deploy

--- app/Blocks/Hero.php
+++ app/Blocks/Hero.php
@@ -38,39 +38,39 @@ class Hero extends Block
 			->addTab('Treść', ['placement' => 'top'])
 			->addGroup('g_hero', ['label' => 'Hero'])
 			->addTrueFalse('use_video', [
-  'label' => 'Użyj wideo w tle',
-  'ui' => 1,
-  'default_value' => 0, // domyślnie obraz
-  'ui_on_text' => 'Tak',
-  'ui_off_text' => 'Nie',
-])
+				'label' => 'Użyj wideo w tle',
+				'ui' => 1,
+				'default_value' => 0, // domyślnie obraz
+				'ui_on_text' => 'Tak',
+				'ui_off_text' => 'Nie',
+			])
 
-->addImage('image', [
-  'label' => 'Obraz',
-  'return_format' => 'array',
-  'preview_size' => 'medium',
-  'conditional_logic' => [
-    [[ 'field' => 'use_video', 'operator' => '!=', 'value' => 1 ]] // pokazuj tylko gdy wideo = off
-  ],
-])
+			->addImage('image', [
+				'label' => 'Obraz',
+				'return_format' => 'array',
+				'preview_size' => 'medium',
+				'conditional_logic' => [
+					[['field' => 'use_video', 'operator' => '!=', 'value' => 1]] // pokazuj tylko gdy wideo = off
+				],
+			])
 
-->addFile('video', [
-  'label' => 'Wideo (MP4/WebM/Ogg)',
-  'return_format' => 'array',
-  'mime_types' => 'mp4,webm,ogv',
-  'conditional_logic' => [
-    [[ 'field' => 'use_video', 'operator' => '==', 'value' => 1 ]]
-  ],
-])
+			->addFile('video', [
+				'label' => 'Wideo (MP4/WebM/Ogg)',
+				'return_format' => 'array',
+				'mime_types' => 'mp4,webm,ogv',
+				'conditional_logic' => [
+					[['field' => 'use_video', 'operator' => '==', 'value' => 1]]
+				],
+			])
 
-->addImage('video_poster', [
-  'label' => 'Poster (obrazek startowy)',
-  'return_format' => 'array',
-  'preview_size' => 'medium',
-  'conditional_logic' => [
-    [[ 'field' => 'use_video', 'operator' => '==', 'value' => 1 ]]
-  ],
-])
+			->addImage('video_poster', [
+				'label' => 'Poster (obrazek startowy)',
+				'return_format' => 'array',
+				'preview_size' => 'medium',
+				'conditional_logic' => [
+					[['field' => 'use_video', 'operator' => '==', 'value' => 1]]
+				],
+			])
 
 			->addText('title', ['label' => 'Tytuł'])
 			->addText('subtitle', ['label' => 'Podtytuł'])
@@ -86,6 +86,23 @@ class Hero extends Block
 				'label' => 'Przycisk #2',
 				'return_format' => 'array',
 			])
+			
+            ->addRepeater('certs', [
+                'label' => 'Certyfikaty',
+                'button_label' => 'Dodaj certyfikat',
+                'layout' => 'table',
+            ])
+                ->addImage('cert_image', [
+                    'label' => 'Obrazek certyfikatu',
+                    'return_format' => 'array',
+                    'preview_size' => 'thumbnail',
+                ])
+                ->addFile('cert_pdf', [
+                    'label' => 'Plik PDF certyfikatu',
+                    'return_format' => 'array',
+                    'mime_types' => 'pdf',
+                ])
+            ->endRepeater()
 
 			->endGroup()
 
